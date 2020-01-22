91c054275b70bcee8008ecb1aa1a0b8e4b283bd0
Mirosław Stasiek <miroslaw.stasiek@neostrada.pl>
2020-01-22 07:46:39 +0100

    Module5 task 2 part 2

--- js/script.js
+++ js/script.js
@@ -36,3 +36,35 @@ else if(playerInput == '3'){
 }
 
 printMessage('Twój ruch to: ' + playerMove);
+
+
+if(computerMove == 'kamień' && playerMove == 'papier'){
+  printMessage('Ty wygrywasz!');
+}
+else if(computerMove == 'kamień' && playerMove == 'kamień'){
+  printMessage('Remis!');
+}
+else if(computerMove == 'kamień' && playerMove == 'nożyce'){
+  printMessage('Przegrywasz!')
+}
+else if(computerMove == 'papier' && playerMove == 'nożyce'){
+  printMessage('Ty wygrywasz!');
+}
+else if(computerMove == 'papier' && playerMove == 'papier'){
+  printMessage('Remis!');
+}
+else if(computerMove == 'papier' && playerMove == 'kamień'){
+  printMessage('Przegrywasz!');
+}
+else if(computerMove == 'nożyce' && playerMove == 'kamień'){
+  printMessage('Ty wygrywasz!');
+}
+else if(computerMove == 'nożyce' && playerMove == 'nożyce'){
+  printMessage('Remis!');
+}
+else if(computerMove == 'nożyce' && playerMove == 'papier'){
+  printMessage('Przegrywasz!');
+}
+else{
+  printMessage('Wpisz cyfrę w przedziale od 1 do 3!')
+}

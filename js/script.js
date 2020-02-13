f949116ac41417cec9f708dc6168ddcb3d0e64c1
Mirosław Stasiek <miroslaw.stasiek@neostrada.pl>
2020-02-13 13:37:18 +0100

    UseFunctionTask

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
+let randomNumber = Math.floor(Math.random() * 3 + 1);
+console.log('Wylosowana liczba to: ' + randomNumber);
+let computerMove = getMoveName(randomNumber);
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+console.log('Gracz wpisał: ' + playerInput);
+let playerMove = getMoveName(playerInput);
+
 function getMoveName(argMoveId){
   if(argMoveId == 1){
     return 'kamień';
@@ -5,14 +12,36 @@ function getMoveName(argMoveId){
     return 'papier';
   } else if(argMoveId == 3){
     return 'nożyce';
+  } else {
+    printMessage('Nie znam ruchu o id' + argMoveId + '.');
+    return 'nieznany ruch';
+    }
   }
+  function displayResult(argComputerMove, argPlayerMove) {
+    printMessage('Zagrałem ' + argComputerMove + ', a Ty '  + argPlayerMove);
+    if (argPlayerMove == 'nieznany ruch') {
+        return 'Niewłaściwy wybór! Wpisz 1, 2 albo 3!';
+    } else if (argComputerMove == 'kamień' && argPlayerMove == 'papier') {
+        return 'Ty wygrywasz!';
+    } else if (argComputerMove == 'kamień' && argPlayerMove == 'nożyce') {
+        return 'Przegrywasz!';
+    } else if (argComputerMove == 'papier' && argPlayerMove == 'nożyce') {
+        return 'Ty wygrywasz!';
+    } else if (argComputerMove == 'papier' && argPlayerMove == "kamień") {
+        return 'Przegrywasz!';
+    } else if (argComputerMove == 'nożyce' && argPlayerMove == 'kamień') {
+        return 'Ty wygrywasz!';
+    } else if (argComputerMove == "nożyce" && argPlayerMove == "papier") {
+        return 'Przegrywasz!';
+    }
+    if (argComputerMove == argPlayerMove) {
+        return "Remis!";
+    }
+    console.log('moves:', argComputerMove, argPlayerMove);
 }
-let randomNumber = Math.floor(Math.random() * 3 + 1);
 
-console.log('Wylosowana liczba to: ' + randomNumber);
-
-let computerMove = getMoveName(argMoveId);
-
+let result = displayResult(computerMove, playerMove);
+printMessage(result);
 /*if(randomNumber == 1){
   computerMove = 'kamień';
 }
@@ -21,20 +50,8 @@ else if(randomNumber == 2){
 }
 else if(randomNumber == 3){
   computerMove = 'nożyce';
-}*/
-
-printMessage('Mój ruch to: ' + computerMove);
-
-
-
-
-let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
-
-console.log('Gracz wpisał: ' + playerInput);
-
-let playerMove = getMoveName(argMoveId);
-
-/*if(playerInput == '1'){
+}
+if(playerInput == '1'){
   playerMove = 'kamień';
 }
 else if(playerInput == '2'){
@@ -42,11 +59,7 @@ else if(playerInput == '2'){
 }
 else if(playerInput == '3'){
   playerMove = 'nożyce';
-}*/
-
-printMessage('Twój ruch to: ' + playerMove);
-
-
+}
 if(computerMove == 'kamień' && playerMove == 'papier'){
   printMessage('Ty wygrywasz!');
 }
@@ -76,4 +89,4 @@ else if(computerMove == 'nożyce' && playerMove == 'papier'){
 }
 else{
   printMessage('Wpisz cyfrę w przedziale od 1 do 3!')
-}
+}*/
